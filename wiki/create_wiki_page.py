52a051bdbe5a3f75750e2e31b4acee492bd92f92
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2015-08-24 23:45:29 +0200

    page for table (+cleaning)

--- wiki/create_wiki_page.py
+++ wiki/create_wiki_page.py
@@ -19,30 +19,49 @@ filename, decalage = 'wbcontac.csv', 0
 file = os.path.join(path_doc, filename)
 
 feuille = pd.read_csv(file, sep=';')
+feuille = feuille[feuille.nom_var.notnull()]
 
-# TODO: faire une page pour un filename
+# Page de la table
+def list_var_to_md_table(code):
+    text = '| Variable | Nom var raccourci | \n|----|----| \n'
+    table = '|' + code['Unnamed: 0'] + '|'
+    text += table
+    return text
+
+text = feuille['Unnamed: 0'] + ' | [[' + \
+        feuille['nom_var_raccourci'] + ']] |'
+text = text.to_string()
+
+text = '## ' + filename[:-4] + '\n Cette table contient les ' + \
+        'variables suivantes : \n \n' + text
+
+path = os.path.join(path_wiki, filename[:-4] + '.md')
+f = open(path, 'w+')
+f.write(text)
+f.close()
+
+
+# Page des variables
 feuille.fillna('', inplace=True)
 
 
 def code_to_md_table(code):
     if code == '':
         return ''
-        
+
     text = '| Code | Label | \n|----|----| \n'
-    table = code[3:].replace('\r\n', '| \n|') 
+    table = code[3:].replace('\r\n', '| \n|')
     table = '|' + table.replace('\t', ' | ') + '|'
     text += table
     return text
 
-for _, row in feuille.iterrows():
-    print row
-    path = os.path.join(path_wiki, row['nom_var'] + '.md') 
-    
 
-        
-    
-    text = row['Description'] + '\n' + '\n' 
+for _, row in feuille.iterrows():
+    text = row['Description'] + '\n' + '\n'
     text += code_to_md_table(row['Codification'])
-    
+
+    path = os.path.join(path_wiki, row['nom_var'] + '.md')
+    print path
     f = open(path, 'w+')
     f.write(text)
+    f.close()
